88ab4f30689f4f00e74e3a93d156d7527fc50f9c
flexbosse <68120738+flexbosse@users.noreply.github.com>
2020-07-13 14:42:49 -0500

    i wanna be blue

--- i wannan be blue/ViewController.swift
+++ i wannan be blue/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var onColor = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var what: UILabel!
+    
+    @IBAction func bluepress(_ sender: Any) {
+        onColor = !onColor
+        UpdateUI()
+    }
+    @IBOutlet weak var lable: UIButton!
+    
+    func UpdateUI() {
+        if onColor{
+            view.backgroundColor = .white
+            lable.setTitle("i wanna be blue", for: .normal)
+            what.text="what color am i?"
+            what.textColor = .black
+         
+            
+        } else {
+            
+            view.backgroundColor =  .blue
+            lable.setTitle("i am blue", for: .normal)
+            what.text = "its hard being green?"
+            what.textColor = .green
+            
+        }
+}
 }
 
